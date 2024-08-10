54cf0c03fdf2508c5971532fbd5196a4b60460a9
muralimallela <mmallela9@gmail.com>
2024-08-10 07:03:57 +0530

    Modal redesigned

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "HIPS : Hetero Institute of Pharmaceutical Sciences",
-  description: "Hetero Institute of Pharmaceutical Sciences",
+  description: "A new star has born in the galaxy of pharmacy colleges in Telangana in the academic year 2007-2008 to guide the students in the areas of wisdom and excellence in pharmacy education. Vipasana Educational Society has established, Hetero Institute of Pharmaceutical Sciences by Sri. B. Parthasaradhi Reddy.",
+  openGraph: {
+    type: "website",
+    url: "https://hips.muralikrishna.online/",
+    title: "HIPS : Hetero Institute of Pharmaceutical Sciences",
+    description: "A new star has born in the galaxy of pharmacy colleges in Telangana in the academic year 2007-2008 to guide the students in the areas of wisdom and excellence in pharmacy education. Vipasana Educational Society has established, Hetero Institute of Pharmaceutical Sciences by Sri. B. Parthasaradhi Reddy.",
+    images: ["https://hips.muralikrishna.online/logo.png"]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "HIPS : Hetero Institute of Pharmaceutical Sciences",
+    description: "A new star has born in the galaxy of pharmacy colleges in Telangana in the academic year 2007-2008 to guide the students in the areas of wisdom and excellence in pharmacy education. Vipasana Educational Society has established, Hetero Institute of Pharmaceutical Sciences by Sri. B. Parthasaradhi Reddy.",
+    images: ["https://hips.muralikrishna.online/logo.png"]
+  }
 };
+
+
 
 export default function RootLayout({
   children,
